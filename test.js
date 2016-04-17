6b1131a1f3bb31d03c1c5e2d7abfc7bd67dee887
Titus Wormer <tituswormer@gmail.com>
2016-04-17 16:37:00 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,139 @@
+/**
+ * @author Titus Wormer
+ * @copyright 2016 Titus Wormer
+ * @license MIT
+ * @module hast:util:is-element
+ * @fileoverview Test suite for `is-element`.
+ */
+
+'use strict';
+
+/* eslint-env node */
+
+/*
+ * Module dependencies.
+ */
+
+var test = require('tape');
+var isElement = require('./index.js');
+
+/*
+ * Tests.
+ */
+
+test('isElement', function (t) {
+    t.equal(isElement(), false, 'should return `false` without node');
+    t.equal(isElement(null), false, 'should return `false` with `null`');
+
+    t.throws(
+        function () {
+            isElement(null, true)
+        },
+        'Expected `string` or `Array.<string>` for `tagNames`, not `true`',
+        'should throw when the second parameter is invalid'
+    );
+
+    t.test('isElement(node)', function (st) {
+        st.equal(isElement({
+            'type': 'text'
+        }), false, 'should return `false` when without `element`');
+
+        st.equal(isElement({
+            'type': 'element'
+        }), false, 'should return `false` when with invalid `element`');
+
+        st.equal(isElement({
+            'type': 'element',
+            'tagName': 'div'
+        }), true, 'should return `true` when with valid `element`');
+
+        st.end();
+    });
+
+    t.test('isElement(node, tagName)', function (st) {
+        st.equal(
+            isElement({
+                'type': 'text'
+            }, 'div'),
+            false,
+            'should return `false` when without `element`'
+        );
+
+        st.equal(
+            isElement({
+                'type': 'element'
+            }, 'div'),
+            false,
+            'should return `false` when with invalid `element`'
+        );
+
+        st.equal(
+            isElement({
+                'type': 'element',
+                'tagName': 'strong'
+            }, 'div'),
+            false,
+            'should return `false` when without matching `element`'
+        );
+
+        st.equal(
+            isElement({
+                'type': 'element',
+                'tagName': 'div'
+            }, 'div'),
+            true,
+            'should return `true` when with matching `element`'
+        );
+
+        st.end();
+    });
+
+    t.test('isElement(node, tagNames)', function (st) {
+        st.equal(
+            isElement({
+                'type': 'text'
+            }, ['div']),
+            false,
+            'should return `false` when without `element`'
+        );
+
+        st.equal(
+            isElement({
+                'type': 'element'
+            }, ['div']),
+            false,
+            'should return `false` when with invalid `element`'
+        );
+
+        st.equal(
+            isElement({
+                'type': 'element',
+                'tagName': 'strong'
+            }, ['div']),
+            false,
+            'should return `false` when without matching `element`'
+        );
+
+        st.equal(
+            isElement({
+                'type': 'element',
+                'tagName': 'div'
+            }, ['div', 'strong', 'em']),
+            true,
+            'should return `true` when with matching `element` (#1)'
+        );
+
+        st.equal(
+            isElement({
+                'type': 'element',
+                'tagName': 'em'
+            }, ['div', 'strong', 'em']),
+            true,
+            'should return `true` when with matching `element` (#2)'
+        );
+
+        st.end();
+    });
+
+    t.end();
+});
